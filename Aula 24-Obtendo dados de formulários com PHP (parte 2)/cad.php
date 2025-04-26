6d04e6f379c1f1fea356d0b2400b5fbec03a7516
Matheus Lemos <matheuslemos573@gmail.com>
2025-04-26 17:30:53 -0300

    Aula 24 finalizada

--- Aula 24-Obtendo dados de formulários com PHP (parte 2)/cad.php
+++ Aula 24-Obtendo dados de formulários com PHP (parte 2)/cad.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -11,8 +12,11 @@
     </header>
     <main>
         <?php 
-            var_dump($_GET)
+            $n = $_GET["nome"] ?? "sem nome";
+            $s = $_GET["sobrenome"] ?? "sem sobrenome";
+            echo "<p>É um prazer te conhecer, $n $s! Este é o meu site"
         ?>
+        <p><a href="javascript:history.go(-1)">Voltar a página anterior</a></p>
     </main>
 </body>
 </html>
