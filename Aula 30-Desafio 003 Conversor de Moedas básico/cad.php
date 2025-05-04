9bfe85fd5f5c1d93d97614e9ac962e2125ea087f
Matheus Lemos <matheuslemos573@gmail.com>
2025-05-03 21:28:52 -0300

    alteração no código PHP

--- Aula 30-Desafio 003 Conversor de Moedas básico/cad.php
+++ Aula 30-Desafio 003 Conversor de Moedas básico/cad.php
@@ -3,25 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
+    <title>Desafio PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <h1>Resultado do Processamento</h1>
-    </header>
     <main>
-        <h1>Resultado Final</h1>
+        <h1>Conversor de Moedas</h1>
         <?php 
-            $n = $_GET["numero"];
-            $valor = $n / 5.65;
-            $valorA = round($valor, 2);
-            echo"<p>Seus R$ $n  equivalem a <strong>US$ $valorA </strong></p>";
-            echo"<p><strong>Cotação fixa de R$5,65</strong> informada diretamente no código.</p>";
+            // Cotação copiada do Google
+            $cotação = 5.17;
+
+            // Quantos $$ você tem ?
+            $real = $_REQUEST["numero"];
+
+            //Equivalência em dólar
+            $dólar = $real / $cotação;
+
+            //Mostra o resultado
+            //echo "Seus R\$" . number_format($real, 2, ",", ".") . " equivalem a US\$" . number_format($dólar, 2, ",", ".");
+
+            //Formatação de moedas com internacionalização!
+            //Biblioteca intl (Internallization PHP)
+
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dólar, "USD") . "</strong> </p>";
         ?>
-        <p>
-            <a href="javascript:history.go(-1)"><input type="submit" value="⬅️Voltar" class="voltar"></a>
-        </p>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 </body>
 </html>
+<!--
+    $n = $_GET["numero"];
+    $valor = $n / 5.65;
+    $valorA = round($valor, 2);
+    echo"<p>Seus R$ $n  equivalem a <strong>US$ $valorA </strong></p>";
+    echo"<p><strong>Cotação fixa de R$5,65</strong> informada diretamente no código.</p>"; 
+-->
