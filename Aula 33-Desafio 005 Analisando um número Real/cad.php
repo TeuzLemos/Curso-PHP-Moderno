029f5fcfa971cdc8b36a2a0f69a1b42c6d1b38e4
Matheus Lemos <matheuslemos573@gmail.com>
2025-05-03 21:31:57 -0300

    Aula 33-Desafio 005 Analisando um número Real

---
+++ Aula 33-Desafio 005 Analisando um número Real/cad.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php
+        $n = $_GET["numero"];
+        $ValorAbsoluto = round($n);
+        $ValorFracionado = $n - floor($n);
+        $ValorF = round($ValorFracionado, 3);
+        echo "<p>Analisando o número <strong>$n</strong> informado pelo usuário</p>";
+        echo "<ul>
+                <li>A parte inteira do número é <strong>$ValorAbsoluto</strong></li>
+                <li>A parte fracionada do número é <strong>$ValorF</strong></li>
+            </ul>";
+        ?>
+        <p>
+            <a href="javascript:history.go(-1)"><input type="submit" value="⬅️Voltar" class="voltar"></a>
+        </p>
+    </main>
+</body>
+
+</html>
