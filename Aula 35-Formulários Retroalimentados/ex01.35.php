35e4bfdccfd3bf47481380bd9210ab483971633e
Matheus Lemos <matheuslemos573@gmail.com>
2025-05-15 08:59:58 -0300

    Aula 35-Formulários Retroalimentados Completa

--- Aula 35-Formulários Retroalimentados/ex01.35.php
+++ Aula 35-Formulários Retroalimentados/ex01.35.php
@@ -15,13 +15,21 @@
     <main>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="v1">
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
 
             <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="v2">
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
 
             <input type="submit" value="Somar">
         </form>
     </main>
+
+    <section id="resultado">
+        <h2>Resultado da Soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            print "<p>A soma entre os valores $valor1 e $valor2 é igual a $soma </p>";
+        ?>
+    </section>
 </body>
 </html>
