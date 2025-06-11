e9b009a11469ef3829a2dde6983fc7168b636904
Matheus Lemos <matheuslemos573@gmail.com>
2025-06-10 21:22:18 -0300

    Aula 40-Entendendo as Médias Completo

--- Aula 40-Entendendo as Médias/ex01.40.php
+++ Aula 40-Entendendo as Médias/ex01.40.php
@@ -4,33 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Interação com Formulários</title>
+    <title>Desafio PHP</title>
 </head>
 <body>
-    <?php 
-        $numero = $_GET['numero'] ?? 0;
-    ?>
-    <header>
-        <h1>Apresente-se para nós</h1>
-    </header>
-    <section>
-        <form action="<?php  echo $_SERVER['PHP_SELF'] ?>" method="get">
-            
-
-            <label for="Número">Número</label>
-            <input type="number" name="numero" id="numero" value="<?=$numero?>">
-
-            <input type="submit" value="Calcular Raízes">
-        </form>
-    </section>
-    <section>
-        <h2>Resultado Final</h2>
+   <main>
         <?php 
-            $raizCubica = pow($numero, 1/3);
-            echo"<p> Analisando o <strong>número $numero</strong> temos: </p>";
-            echo"<li> A sua raiz quadrada é " . "<strong>" . sqrt($numero) . "</strong>";
-            echo"<li> A sua raiz quadrada cúbica é " . "<strong>" . $raizCubica . "</strong>";
+            $valor1 = $_GET['v1'] ?? '';
+            $peso1 = $_GET['p1'] ?? '' ;
+            $valor2 = $_GET['v2'] ?? '';
+            $peso2 = $_GET['p2'] ?? '' ;
         ?>
-    </section>
+        <h1>Médias Aritméticas</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="v1">1º Valor</label>
+            <input type="number" name="v1" id="v1" required value="<?=$valor1?>">
+            <label for="p1">1º Peso</label>
+            <input type="number" name="p1" id="p1" required value="<?=$peso1?>">>
+
+            <label for="v2">2º Valor</label>
+            <input type="number" name="v2" id="v2" required value="<?=$valor2?>">>
+            <label for="p2">2º Peso</label>
+            <input type="number" name="p2" id="p2" required value="<?=$peso2?>">>
+
+            <input type="submit" value="Calcular Médias">
+        </form>
+   </main>
+   <section>
+        <?php 
+            $ma = ($valor1 + $valor2) / 2;
+            $mp = ($valor1 * $peso1 + $valor2 * $peso2)/($peso1 + $peso2);
+        ?>
+        <h2>Cálculo das Médias</h2>
+        <p>Analisando os valores <?=$valor1 ?> e <?=$valor2 ?>:</p>
+        <ul>
+            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a <?=number_format($ma, 2, ",", "." ) ?></li>
+            <li>A <strong>Média Aritmética Ponderada</strong> com os pesos <?=$peso1 ?> e <?=$peso2?> é igual a <?=number_format($mp, 2, ",", ".")?></li>
+        </ul>
+   </section>
 </body>
 </html>
